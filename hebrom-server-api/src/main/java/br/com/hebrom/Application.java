872f119ac964d885fba1f4ca3934c6796e7732c4
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2020-10-17 09:42:38 -0300

    feat: estrutura projeto

--- hebrom-server-api/src/main/java/br/com/hebrom/Application.java
+++ hebrom-server-api/src/main/java/br/com/hebrom/Application.java
@@ -2,12 +2,19 @@ package br.com.hebrom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+
+import br.com.hebrom.propertie.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ FileStorageProperties.class })
+@PropertySource(value = { "classpath:application.properties" })
+@PropertySource(value = { "file:${conf.properties:install/conf.properties}" })
 public class Application {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(Application.class, args);
-	}
+        SpringApplication.run(Application.class, args);
+    }
 }
