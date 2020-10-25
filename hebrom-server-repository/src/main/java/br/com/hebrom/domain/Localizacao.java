5f09f8461df3a08e4d57915067a5c3e3b2a30edd
Secco <louizoliveira03@hotmail.com>
2020-10-25 10:53:44 -0300

    feat: Criado domains

---
+++ hebrom-server-repository/src/main/java/br/com/hebrom/domain/Localizacao.java
@@ -0,0 +1,35 @@
+package br.com.hebrom.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_localizacao")
+public class Localizacao {
+
+	@Id
+	@Column(name = "id_localizacao")
+	@GeneratedValue(generator = "seq_localizacao")
+	private Long id;
+
+	@Column(name = "tx_localizacao")
+	private String localizacao;
+
+	@Column(name = "tx_observacao")
+	private String observacao;
+
+	@Column(name = "tx_goolgemaps")
+	private String googleMaps;
+
+}
