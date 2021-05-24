d362a8860d4303994e4ba4c32c398f4ed25e913e
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2021-05-24 20:12:10 -0300

    coisas

--- hebrom-server-api/src/main/java/br/com/hebrom/Application.java
+++ hebrom-server-api/src/main/java/br/com/hebrom/Application.java
@@ -1,9 +1,12 @@
 package br.com.hebrom;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.hebrom.config.FileStorageProperties;
 
@@ -11,6 +14,9 @@ import br.com.hebrom.config.FileStorageProperties;
 @EnableConfigurationProperties({ FileStorageProperties.class })
 @PropertySource(value = { "classpath:application.properties" })
 @PropertySource(value = { "file:${conf.properties:install/conf.properties}" })
+@EnableTransactionManagement
+@RestController
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
